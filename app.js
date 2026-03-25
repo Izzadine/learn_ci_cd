da95c157a28a3d2e8276f20797f3e66cb13946d2
Ahmat Izzedine <96932196+Izzadine@users.noreply.github.com>
2026-03-25 09:24:25 +0100

    fixed format warning

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const msg = 'Hello world!'
+  const msg = 'Hello world!';
   res.send(msg);
-
 });
 
 module.exports = app;
